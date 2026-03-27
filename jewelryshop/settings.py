cb8b04e622045f864e133af2a8daa7a0d76baede
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-03-27 20:45:32 +0530

    remove localhost mysql and use railway mysql

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 import dj_database_url
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
