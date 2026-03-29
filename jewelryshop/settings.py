56068e2abe5a7ec2e64e93eb08aa57bb683c3ade
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-03-29 13:27:48 +0530

    Add CSRF_TRUSTED_ORIGINS for Railway domain

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -109,6 +109,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #setting email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS= True
 EMAIL_PORT= 587
