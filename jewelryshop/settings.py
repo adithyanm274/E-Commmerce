a7e7912231f7688873e0487dd20bd7a7243054dd
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-03-30 20:44:14 +0530

    Add DEFAULT_FROM_EMAIL and fix email settings

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -115,15 +115,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#setting email
+# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_USE_TLS= True
-EMAIL_PORT= 587
-EMAIL_HOST_USER='adithyan.m.2742001@gmail.com'
-EMAIL_HOST_PASSWORD= 'rvjg cual cckp ltim'
-
-
-# Add this to test with a different email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'adithyan.m.2742001@gmail.com'
+EMAIL_HOST_PASSWORD = 'rvjg cual cckp ltim'  # Your app password
 DEFAULT_FROM_EMAIL = 'adithyan.m.2742001@gmail.com'
 SERVER_EMAIL = 'adithyan.m.2742001@gmail.com'
