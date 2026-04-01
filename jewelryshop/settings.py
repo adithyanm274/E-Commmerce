fbca09d726e5da39655243c0e4474026889a0706
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-04-01 23:43:45 +0530

    error clear

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -120,11 +120,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings - SMTP
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True          # instead of EMAIL_USE_TLS
-EMAIL_HOST_USER = 'adithyan.m.2742001@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'   # literal string "apikey"
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'adithyan.m.2742001@gmail.com'
