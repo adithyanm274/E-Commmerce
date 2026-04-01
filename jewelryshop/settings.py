2617336848c43fc73a41499bbdb83809c608a84f
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-04-01 23:23:38 +0530

    error clear

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -120,6 +120,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings - SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
