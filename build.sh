46afc75da2478dce417c4a1c57d8e0cd0bf7c214
adithyanm274 <adithyan.m.2742001@gmail.com>
2026-04-03 15:42:20 +0530

    add build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
